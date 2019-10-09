fa6d25beb57fe039f17d46b0d84826ee4809cad8
Mona Mostafa <795911@seq.org>
2019-10-08 22:30:30 -0700

    edit

--- Lab 107 Checkerboard/sketch.js
+++ Lab 107 Checkerboard/sketch.js
@@ -1,30 +1,40 @@
-/*
-**Lab 1007 Ball
-**Checker Board
-**
-*/
+//Mona Mostafa
+//Lab 1007 Ball
+//Checker Board
 
+var squares=[];
 
-//  Global variables
-var squares = []  // declare b1 as a global variable
-
-
-// put setup code here
-function setup() {
-  var cnv = createCanvas(800, 800);
-  cnv.position((windowWidth-width)/2, 30);
+function setup(){
+  var cnv=createCanvas(800,800);
+  cnv.position((windowWidth-width)/2,30);
   background(235);
   loadSquares();
-  //  traverse array and run each ball
-  for(var i = 0; i < squares.length; i++){
-   squares[i].render();
+  //traverse array
+  for(var i=0;i<squares.length;i++){
+    squares[i].render();
   }
 }
 
-
 function loadSquares(){
-
-}
-
-
+  for (var i=0;i<8;i++){
+    for(var x=0;x<8;x++){
+      if(i%2===0){
+        if(x%2===0){
+          var shade=color(70, 5, 18);
+        }
+        if(x%2!==0){
+          var shade=color(255);
+        }
+      }
+      if(i%2!==0){
+        if(x%2!==0){
+          var shade=color(70, 5, 18);
+        }
+        if(x%2===0){
+          var shade=color(255;
+        }
+      }
+      squares[x+(8*i)]= new Square(100*x,100*i,100,100, shade);
+    }
+  }
 }
