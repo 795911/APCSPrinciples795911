253c8c3be76361dbfcce6e5efdd25a0e1525a770
Mona Mostafa <795911@seq.org>
2020-01-29 23:50:24 -0800

    boid

---
+++ Art/sketch.js
@@ -0,0 +1,30 @@
+//  Mona Mostafa
+// 	1-28-19
+//  This is a comment
+//  The setup function function is called once when your program begins
+var boids = [];
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+  loadBoids(30);
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+  background(5, 5, 5, 5);//makes black transparent background
+  runBoids();//runs the boid function
+}
+
+function loadBoids(x){
+  for(var i = 0; i < x; i++){//creates the boids
+    boids[i] = new Boid(random(100, 700), random(100, 700), random(-2, 2), random(-2, 2));
+  }
+}
+
+function runBoids(){//runs the boids
+  for(var i = 0; i < boids.length; i++){
+    boids[i].run();
+  }
+}
